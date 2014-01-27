2007548c4872753e2e64a2d5c2dd9492e75cb4b3
renemonroy <rene.monroy@gmail.com>
2014-01-27 16:10:52 -0600

    Update children module

--- examples/views/Children.js
+++ examples/views/Children.js
@@ -8,18 +8,26 @@ Module(App.Helpers, 'Children')({
  
     currentPos : 0,
 
+    /**
+     * Dispatches the new and old context of children. The goal is to not do
+     * DOM manipulation and let the main Class take care of it.
+    **/
     goTo : function goTo(index) {
       var myClass = this;
       if ( typeof index === 'number' && index !== myClass.currentPos ) {
-        myClass.dispatch('goTo', {
-          currentView : myClass.children[myClass.currentPos],
-          newView : myClass.children[index]
+        myClass.dispatch('children:onGoTo', {
+          currentChild : myClass.children[myClass.currentPos],
+          newChild : myClass.children[index]
         });
         myClass.currentPos = index;
       }
       return this;
     },
 
+    /**
+     * Uses goTo to change context to the next child. It changes to the first
+     * child if the last context was the last child.
+    **/
     next : function next() {
       var myClass = this;
       if ( myClass.currentPos < (myClass.children.length - 1) ) {
@@ -30,6 +38,10 @@ Module(App.Helpers, 'Children')({
       return this;
     },
 
+    /**
+     * Uses goTo to change context to a previous child in the array. It changes
+     * to the last one if the context was the first child.
+    **/
     prev : function prev() {
       var myClass = this;
       if ( myClass.currentPos > 0 ) {
@@ -37,6 +49,29 @@ Module(App.Helpers, 'Children')({
       } else {
         myClass.goTo(myClass.children.length - 1);
       }
+      return this;
+    },
+
+    /**
+     * Removes a child class from the parent given a string 'name'. The element
+     * will be removed from DOM too.
+    **/
+    remove : function remove(name) {
+      var myClass = this; 
+      myClass[name].destroy();
+      return this;
+    },
+
+    /**
+     * Creates a new child Class instance with a random name. It does not
+     * render and should does it manually.
+    **/
+    create : function create(instance, el) {
+      var myClass = this, newInstance;
+      newInstance = myClass.appendChild( new instance({
+        name : Math.random().toString().replace('.', '').substr(0, 24),
+        element : el ? el : instance.HTML
+      }));
       return this;
     }
   
--- examples/views/ViewsManager.js
+++ examples/views/ViewsManager.js
@@ -21,32 +21,17 @@ Class(App.UI, 'ViewsManager').inherits(App.Widget).includes(App.Helpers.Children
      * have them in one place is for organization.
     **/
     _bindEvents : function _bindEvents() {
-      this.bind('goTo', function(views) {
-        views.currentView.deactivate();
-        views.newView.activate();
+
+      var viewsManager = this;
+
+      /** 
+       * When child context changes
+      **/
+      viewsManager.bind('children:onGoTo', function(views) {
+        views.currentChild.deactivate();
+        views.newChild.activate();
       });
-    },
 
-    /**
-     * Creates a new View Widget with a random name and renders it in
-     * Views Manager element.
-    **/
-    create : function create() {
-      var viewsManager = this, newView;
-      newView = viewsManager.appendChild( new App.UI.View({
-        name : 'view_' + Math.random().toString().replace('.', '').substr(0,24)
-      }));
-      newView.render(viewsManager.element);
-      return this;
-    },
-
-    /**
-     * Removes a View from the manager from a string 'name'. The element
-     * will be removed from DOM too.
-    **/
-    remove : function remove(viewName) {
-      this[viewName].destroy();
-      return this;
     }
 
   }
