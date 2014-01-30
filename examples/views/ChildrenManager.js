d1d32ff2e52ca3c1eecd172fb53a703b22ad61b9
renemonroy <rene.monroy@gmail.com>
2014-01-30 11:03:34 -0600

    Change names

--- examples/views/ChildrenManager.js
+++ examples/views/ChildrenManager.js
@@ -6,68 +6,68 @@ Module(App.Helpers, 'ChildrenManager')({
 
   prototype : {
  
-    currentPos : 0,
+    currentChildPos : 0,
 
     /**
      * Dispatches the new and old context of children. The goal is to not do
      * DOM manipulation and let the main Class take care of it.
     **/
-    goTo : function goTo(index) {
+    getChild : function getChild(index) {
       var myClass = this, myChild;
-      if ( typeof index === 'number' && index !== myClass.currentPos ) {
+      if ( typeof index === 'number' && index !== myClass.currentChildPos ) {
         myChild = myClass.children[index];
         myClass.dispatch('child:onChange', {
-          oldChild : myClass.children[myClass.currentPos],
+          oldChild : myClass.children[myClass.currentChildPos],
           newChild : myChild
         });
-        myClass.currentPos = index;
+        myClass.currentChildPos = index;
       }
       return myChild;
     },
 
     /**
-     * Uses goTo to change context to the next child. It changes to the first
+     * Uses getChild to change context to the next child. It changes to the first
      * child if the last context was the last child.
     **/
-    next : function next() {
+    getNextChild : function getNextChild() {
       var myClass = this, myChild;
-      if ( myClass.currentPos < (myClass.children.length - 1) ) {
-        myChild = myClass.goTo(myClass.currentPos + 1);
+      if ( myClass.currentChildPos < (myClass.children.length - 1) ) {
+        myChild = myClass.getChild(myClass.currentChildPos + 1);
       } else {
-        myChild = myClass.first();
+        myChild = myClass.getFirstChild();
       }
       return myChild;
     },
 
     /**
-     * Uses goTo to change context to a previous child in the array. It changes
+     * Uses getChild to change context to a previous child in the array. It changes
      * to the last one if the context was the first child.
     **/
-    prev : function prev() {
+    getPrevChild : function getPrevChild() {
       var myClass = this, myChild;
-      if ( myClass.currentPos > 0 ) {
-        myChild = myClass.goTo(myClass.currentPos - 1);
+      if ( myClass.currentChildPos > 0 ) {
+        myChild = myClass.getChild(myClass.currentChildPos - 1);
       } else {
-        myChild = myClass.last();
+        myChild = myClass.getLastChild();
       }
       return myChild;
     },
     
     /**
-     * Uses goTo to change context to last child in the array.
+     * Uses getChild to change context to last child in the array.
     **/
-    first : function first() {
+    getFirstChild : function getFirstChild() {
       var myClass = this, myChild;
-      myChild = myClass.goTo(0);
+      myChild = myClass.getChild(0);
       return myChild;
     },
 
     /**
-     * Uses goTo to change context to last child in the array.
+     * Uses getChild to change context to last child in the array.
     **/
-    last : function last() {
+    getLastChild : function getLastChild() {
       var myClass = this, myChild;
-      myChild = myClass.goTo(myClass.children.length - 1);
+      myChild = myClass.getChild(myClass.children.length - 1);
       return myChild;
     },
 
@@ -75,11 +75,11 @@ Module(App.Helpers, 'ChildrenManager')({
      * Removes a child class from the parent. The element will be removed from DOM
      * too. Events are added to help.
     **/
-    remove : function remove(myChild) {
+    destroyChild : function destroyChild(myChild) {
       var myClass = this;
-      myClass.bind('child:beforeDestroy', { removedChild : myChild });
+      myClass.dispatch('child:beforeDestroy', { destroyedChild : myChild });
       myChild.destroy();
-      myClass.bind('child:afterDestroy', { removedChild : myChild });
+      myClass.dispatch('child:afterDestroy', { destroyedChild : myChild });
       return this;
     },
 
@@ -87,7 +87,7 @@ Module(App.Helpers, 'ChildrenManager')({
      * Creates a new child Class instance with a random name. It does not
      * render and should does it manually.
     **/
-    create : function create(instance, config) {
+    createChild : function createChild(instance, config) {
       var myClass = this, newChild;
       config = config || {};
       if ( !config.name ) {
