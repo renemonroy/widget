2878cc007c220fe07e0b0a5c631ec2fdad5a184d
renemonroy <rene.monroy@gmail.com>
2014-04-03 13:10:48 -0600

    Image cropper update

--- examples/img-cropper/img-cropper.js
+++ examples/img-cropper/img-cropper.js
@@ -1,55 +1,92 @@
-Class(App.UI, 'ImageCropper').inherits(App.Widget)({
+Class(App.UI, 'StageCropper').inherits(App.Widget)({
 
-	ELEMENT_CLASS : 'image-cropper',
+	ELEMENT_CLASS : 'stage-cropper',
 
-	HTML : '<div><canvas style="border: 1px solid #333;"></canvas></div>',
+	HTML : '<canvas></canvas>',
 
 	prototype : {
 
-		canvas : null,
-		image : null,
+		/**
+		 * Initial cropper variables
+		**/
+		imageSrc : null,
+		screenHeight : 300,
+		screenWidth : 300,
+		screenPosX : 50,
+		screenPosY : 50,
+		maskColor : 'rgba(255, 255, 255, .75)',
 
 		/**
-		 * Inital configuration and methods
+		 * Constructor that handles widget configuration
 		**/
 		init : function init(config) {
-			App.Widget.prototype.init.call(this, config);
-			var imgCropper = this;
-
-			imgCropper.bindEvents();
+			var cropper = this;
+			App.Widget.prototype.init.call(cropper, config);
+			cropper._bindEvents();
 		},
 
 		/**
 		 * Bind custom events, used at init
+		 * - Configure canvas once widget is rendered
 		**/
-		bindEvents : function bindEvents() {
-			var imgCropper = this;
-
-			imgCropper.bind('render', function() {
-				imgCropper._setCroppingArea();
-				// imgCropper._handleEventListeners();
+		_bindEvents : function _bindEvents() {
+			var cropper = this;
+			cropper.bind('render', function() {
+				cropper.loadImage(cropper.imageSrc);
 			});
 		},
 
 		/**
-		 * Cropping area configuration
+		 * Creates a new image to crop by getting the
+		 * source set in the config instantiation
 		**/
-		_setCroppingArea : function _setCroppingArea() {
-			var imgCropper = this,
-				canvasEl = imgCropper.element.getElementsByTagName('canvas')[0],
-				ctx = canvasEl.getContext('2d'),
-				img = imgCropper.image;
-
-			imgCropper.canvas.ctx = ctx;
-			imgCropper.canvas.ctx.drawImage(img.file, coordX, coordY, img.width, img.height);
+		loadImage : function loadImage(src) {
+			var cropper = this,
+				img = new Image();
+			img.onload = function () {
+				cropper.canvasImg = img;
+				cropper.setupCanvas();
+			};
+			img.src = src;
 		},
 
 		/**
-		 * Handle dom event listeners
+		 * Configure canvas element and context
+		 * - Sets canvas size depending on parent element
+		 * - Draws a mask on context
 		**/
-		_handleEventListeners : function _handleEventListeners() {
-			var imgCropper = this,
-				cropperEl = imgCropper.element;
+		setupCanvas : function setupCanvas() {
+			var cropper = this,
+				canvasEl  = cropper.element,
+				img = cropper.canvasImg,
+				ctx;
+
+			cropper.canvasCtx = canvasEl.getContext('2d');
+			ctx = cropper.canvasCtx;
+			canvasEl.width = canvasEl.parentNode.offsetWidth;
+			canvasEl.height = canvasEl.parentNode.offsetHeight;
+
+			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+			ctx.drawImage(img, 0, 0, img.width, img.height);
+			ctx.fillStyle = cropper.maskColor;
+			ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+			ctx.strokeStyle = '#000';
+			ctx.lineWidth = 2;
+
+			ctx.strokeRect(
+				cropper.screenPosX,
+				cropper.screenPosY,
+				cropper.screenWidth,
+				cropper.screenHeight
+			);
+		},
+
+		/**
+		 * Repaint the image in the screen depending its
+		 * positioning and zoom
+		**/
+		refreshScreen : function refreshScreen() {
+
 		}
 
 	}
